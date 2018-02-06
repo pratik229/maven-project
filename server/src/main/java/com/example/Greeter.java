7588c63612b6fbf9b9f501556a01a0d514616db3
Pratik Patel <pratik229@gmail.com>
2018-02-06 14:51:10 -0500

    Removing checkstyle errors.

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+  * @param parameter to the function
+  * @return return compute value
+  */
+  public final String greet(final String someone) {
     return String.format("Hello worldwa!!!, %s!", someone);
   }
 }
